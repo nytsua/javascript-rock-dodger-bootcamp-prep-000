78c822ef5a4fcd7b4dbdebeb952b7956ed5144e0
Austyn Trace <atrace@gmail.com>
2017-09-04 21:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-   GAME.append(rock);
+   rock.appendTo(GAME);
    moveRock();
 
   /**
