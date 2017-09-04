cffac84da597a6dd8e505c636c8005cb7b33c3af
Austyn Trace <atrace@gmail.com>
2017-09-04 22:26:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if(e.which === LEFT_ARROW){
+     e.preventDefault();
      moveDodgerLeft();
    } else if(e.which === RIGHT_ARROW){
      moveDodgerRight();
